211845449943f4d0e8987f2199e33c456b8896c4
misterfoxy <michaelscottfox1@gmail.com>
2019-03-11 22:06:52 -0700

    serve basic at index

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3004
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/test', (req, res) => {
 	res.send(req.body.challenge)
+})
+
+app.get('/', (req, res) => {
+	res.send('<h1>Ben Kim + Michael Fox</h1>')
 })
 
 app.listen(port, () => {
