b3c54f9891b4ffadc4b39fd24dbb98df962a0ba0
misterfoxy <michaelscottfox1@gmail.com>
2019-03-11 22:44:57 -0700

    updates to prod

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3004
+const port = 8888;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/test', (req, res) => {
-	res.send(req.body.challenge)
+app.post('/', (req, res) => {
+        console.log(req.body)
+        res.send(req.body)
 })
-
+app.post('/slasher', (req, res) => {
+        console.log(req.body)
+        res.send(req.body)
+})
 app.get('/', (req, res) => {
-	res.send('<h1>Ben Kim + Michael Fox</h1>')
+        res.send('<h1>Ben Kim + Michael Fox</h1>')
 })
 
 app.listen(port, () => {
-	console.log(`Listening on Port ${port}!`)
+        console.log(`Listening on Port ${port}!`)
 });
+~
